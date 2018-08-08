6e0566b01c93a4e25e1c22046db02e521a02991d
Juliette <julietteandrea1@gmail.com>
2018-08-08 06:57:41 +0000

    fixed pagination for profile table, input btn

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -7,3 +7,26 @@ function mypw() {
         x.type = "password";
     }
 }
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var i = 0;
+var txt = 'Welcome to Trifecta!';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
--- static/js/profile.js
+++ static/js/profile.js
@@ -1,29 +1,3 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 // Delete button in table
 function msg(evt) {
     alert("Call Deleted");
@@ -37,7 +11,7 @@ $(document).ready(
         //Create a DataTable
         var data_table = $('#calllog_table').DataTable(
             {
-                "order": [[ 0, "desc" ]]
+                "order": [[ 0, "asc" ]]
             });
         //var column = data_table.columns(0);
         //column.visible(false);
